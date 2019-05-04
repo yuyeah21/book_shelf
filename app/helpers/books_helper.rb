d58c16854cba571f9da3889194e1575cb2555234
“yuyeah21” <yuyeah21@gmail.com>
2019-05-03 21:35:10 -0500

    commit

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,7 @@
 module BooksHelper
   def show_book_image(book)
     if book.image.attached?
-      image_tag @book.image.variant(resize: "300x300"), class: "img-thumbnail"
+      image_tag book.image.variant(resize: "300x300"), class: "img-thumbnail"
     else
       image_tag "no_image.png", class: "img-thumbnail"
     end
