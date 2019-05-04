f9afe6b7ca2c2376ced6ca4e66c2d0f02da38c53
“yuyeah21” <yuyeah21@gmail.com>
2019-05-03 21:55:23 -0500

    コミット

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all
   end
@@ -8,7 +9,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    set_book
     if @book.save
       redirect_to @book, notice: "書籍を登録しました。"
     else
@@ -17,15 +18,15 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    set_book
   end
 
   def edit
-    @book = Book.find(params[:id])
+    set_book
   end
 
   def update
-    @book = Book.find(params[:id])
+    set_book
     if @book.update(book_params)
       redirect_to @book, notice: "書籍を更新しました。"
     else
@@ -33,7 +34,7 @@ class BooksController < ApplicationController
     end
   end
   def destroy
-    @book = Book.find(params[:id])
+    set_book
     @book.destroy
     redirect_to books_path, notice: "書籍を登録しました"
   end
@@ -42,5 +43,8 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
+  end
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
