31f78961e1807d428c3f0c896da79ac4ba297d64
“yuyeah21” <yuyeah21@gmail.com>
2019-05-03 21:09:36 -0500

    削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
   def new
     @book = Book.new
   end
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -13,10 +15,31 @@ class BooksController < ApplicationController
       render :new
     end
   end
+
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を登録しました"
+  end
+
   private
+
   def book_params
     params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
   end
